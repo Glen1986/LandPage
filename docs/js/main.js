f7bb562cd142e913cfc477e214281e0ebff9495e
Glen Escobar <escobar.glen@gmail.com>
2021-01-12 21:01:32 -0300

    21:00 12/01

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,51 +1,17 @@
-/*
-const grid = Muuri('.grid', {
-layout:{
-rounding: false
-}
-});
+$(function(){
+  $('nav.mobile-menu').click(function(){
+    var listaMenu = $('nav.mobile-menu ul')
 
-window.addEventListener('load',() => {
-document.getElementById('grid').classList.add('imagenes-cargadas');
-});
-*/
-window.onload = function(){
-  var map;
 
-  function initialize() {
-    var mapProp = {
-      center: new google.maps.LatLng(-23.5464,-46.6461),
-      zoom: 15,
-      mapTypeId:google.maps.MapTypeId.ROADMAP
+if(listaMenu.is(':hidden') == true){
+  listaMenu.css('display', 'block');
+  console.log('ok');
+ }else{
+      listaMenu.css('display', 'none');
+      console.log('off');
     }
-    map = new google.maps.Map(document.getElementById("map"), mapProp);
-  }
-  function addMarker(lat,long,icon,content,click){
-    var latLng = {'lat':lat, 'lng': long};
+  })     
+})
 
-    var marker = new google.maps.Marker({
-      position:latLng,
-      map:map,
-      icon:icon
-    });
 
-    var infoWindow = new google.maps.InfoWindow({
-      content:content,
-      maxWidth: 200,
-      pixelOffset: new google.maps.Size(0,20)
-    });
 
-    if(click== true){
-    
-    google.maps.event.addListener(marker,'click',function(){
-      infoWindow.open(map,marker);
-      });
-
-    }else{
-      infoWindow.open(map,marker);
-    }
-  }
-  initialize();
-  var contenido = '<p style="color:black;font-size:13px;padding:10px 0;">Meu Endereço</p>';  
-  addMarker(-23.5464,-46.6461,'',contenido,true);
-}
